e865975515145b83b9ec539032eb24ff58ac3228
Carla <carla@layc-dc.org>
2020-11-16 16:27:15 -0500

    inserted queries to populate burgers table

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
+INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
