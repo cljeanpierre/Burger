c9c2e35b185ca137e0486309487272b4c83b4a5c
Carla <carla@layc-dc.org>
2020-12-18 17:33:18 -0500

    added a helper function for SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,15 @@
 //Import MySQL connection
 var connection = require("../config/connection.js");
 
+
+function printQuestionMarks(num) {
+    var arr = [];
+  
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+  
+    return arr.toString();
+  }
 // Export the orm object for the model burger.js
 module.exports = orm;
