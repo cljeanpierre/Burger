4cdf387eece4095eb75bab37f49ff45a1b7fcda3
Carla <carla@layc-dc.org>
2021-01-11 07:16:49 -0500

    changed port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 
-var PORT = process.env.PORT || 5500;
+var PORT = process.env.PORT || 3000;
 
 var app = express();
 
@@ -25,5 +25,5 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function() {
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("Server listening on: http://localhost: " + PORT);
 });
