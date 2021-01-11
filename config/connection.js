3528212fe43cdb2cead83a40acc03ff62ca258e3
Carla <carla@layc-dc.org>
2021-01-11 03:50:23 -0500

    set up JAWDB provision

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 5500,
-  user: "root",
-  password: "Iluvbugs2!!",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 5500,
+    user: "root",
+    password: "Iluvbugs2!!",
+    database: "burgers_db"
 });
+};
 
 // Make the connection.
 connection.connect(function(err) {
