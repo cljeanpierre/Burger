4a27758b31c6c8235301127a02c74319e538c0c0
Carla <carla@layc-dc.org>
2021-01-13 06:09:51 -0500

    modified seeds data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
 INSERT INTO burgers (name) VALUES ('Impossible Burger');
-INSERT INTO burgers (name, devour) VALUES ('Grilled Chicken', true);
-INSERT INTO burgers (name, devour) VALUES ('Cheeseburger', true);
-INSERT INTO burgers (name, devour) VALUES ('Hamburger', true);
+INSERT INTO burgers (name) VALUES ('Grilled Chicken');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Hamburger');
 INSERT INTO burgers (name) VALUES ('Turkey Burger');
