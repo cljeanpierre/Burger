d6385d62e374e7b3e04b151e0b1ccc227d0e9661
Carla <carla@layc-dc.org>
2021-01-13 05:49:52 -0500

    renamed column headings for consistency in code

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Impossible Burger');
-INSERT INTO burgers (burger_name, devoured) VALUES ('Grilled Chicken', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Hamburger', true);
-INSERT INTO burgers (burger_name) VALUES ('Turkey Burger');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Impossible Burger');
+INSERT INTO burgers (name, devour) VALUES ('Grilled Chicken', true);
+INSERT INTO burgers (name, devour) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, devour) VALUES ('Hamburger', true);
+INSERT INTO burgers (name) VALUES ('Turkey Burger');
