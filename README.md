7fad12ef35fad47b4136d9f0bfc48acd01d61fcd
Carla <carla@layc-dc.org>
2021-01-13 06:44:39 -0500

    added project description, deleted unused styles, and moved code block to make submit button work

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Burger
+# Burgers: To Eat or Not To Eat
+
+This application allows users to log and submit the names of burgers they would like to eat.  Whenever a user submits the name of a burger, that burger appears on the left side of the page waiting to be devoured.  When the burger is devoured, it appears on the right side of the page.  All burgers are stored in a database.
+
+This application follows the MVC design pattern and uses Node, Express and MySQL to query and route data.  Handlebars is used to generate the HTML page.
--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,6 +1,32 @@
 //Wait to attach handlers until the DOM is fully loaded.
 
 $(function () {
+
+    $(".create-form").on("submit", function (event) {
+        event.preventDefault();
+        //Always preventDefault on a submit event.
+        var newBurger = {
+            name: $("#bur").val().trim()
+            // devour: $("[name=devour]:checked").val().trim()
+        };
+        console.log (newBurger);
+
+        //Send POST request 
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger")
+                //Reload the page to get the updated list
+                location.reload();
+            }
+        );
+    });
+
+
+
+
     $(".eat-burger").on("click", function (event) {
         var id = $(this).data("id");
         var confirmEat = $(this).data("neweaten") === false;
@@ -8,7 +34,8 @@ $(function () {
         var confirmEatState = {
             devour: confirmEat
         };
-
+        console.log("id" + id + "eaten: " + confirmEatState.devour);
+        
         //Send PUT request
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
@@ -22,25 +49,7 @@ $(function () {
         );
     });
 
-    $(".create-form").on("submit", function (event) {
-        event.preventDefault();
-        //Always preventDefault on a submit event.
-        var newBurger = {
-            name: $("#bur").val().trim(),
-            devour: $("[name=devour]:checked").val().trim()
-        };
-        //Send POST request to "api/burgers" route
-        $.ajax("/api/burgers", {
-            type: "POST",
-            data: newBurger
-        }).then(
-            function() {
-                console.log("created new burger")
-                //Reload the page to get the updated list
-                location.reload();
-            }
-        );
-    });
+
 
     $(".delete-burger").on("click", function(event) {
         var id = $(this).data("id");
