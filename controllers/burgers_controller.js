101ee2298341a52bc3ea5e50d763df1d6c39c7bf
Carla <carla@layc-dc.org>
2021-01-13 05:47:00 -0500

    changed devoured to devour and renamed table column for consistency

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -18,9 +18,9 @@ router.get("/", function(req, res) {
   
   router.post("/api/burgers", function(req, res) {
     burger.create([
-      "burger_name", "devoured"
+      "name", "devour"
     ], [
-      req.body.name, req.body.devoured
+      req.body.name, req.body.devour
     ], function(result) {
 
       // Send back the ID of the new quote
@@ -34,11 +34,11 @@ router.get("/", function(req, res) {
     console.log("condition", condition);
   
     burger.update({
-      devoured: req.body.devoured
+      devour: req.body.devour
     }, condition, function(result) {
       if (result.changedRows == 0) {
 
-        // If no rows were changed, then the ID must not exist, throw 404
+        // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
